4c2dd0725e1868817389ab755907481f0ef125a7
foyreet <foyreet@yandex.ru>
2025-01-22 16:22:08 +0300

    Реализация быстрой сортировки

---
+++ algorithms/quick_sort/quick_sort.h
@@ -0,0 +1,8 @@
+
+#ifndef VK_MADE_QUICK_SORT_H
+#define VK_MADE_QUICK_SORT_H
+#include <vector>
+int split(std::vector<int>& a, int l, int r, int x);
+
+void qsort(std::vector<int>& a, int l, int r);
+#endif //VK_MADE_QUICK_SORT_H
