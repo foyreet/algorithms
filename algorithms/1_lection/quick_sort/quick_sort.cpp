a8e4b0ca2be107b8fa683bd3d3bfcc74c919add8
foyreet <foyreet@yandex.ru>
2025-01-23 14:46:33 +0300

    Разбирался с ошибками

---
+++ algorithms/1_lection/quick_sort/quick_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+//int split(vector<int>& a, int l, int r, int x) {
+//    int support_element = l;
+//    for (int i = l; i < r; i++) {
+//        if (a[i] == x) {
+//            support_element = i;
+//            break;
+//        }
+//    }
+//    swap(a[support_element], a[r-1]);
+//    int m = l;
+//    for (int i = l; i < r-1; i++) {
+//        if (a[i] < x) {
+//            swap(a[i], a[m]);
+//            m++;
+//        }
+//    }
+//    swap(a[m], a[r-1]);
+//    return m;
+//}
+
+//void qsort(vector<int>& a, int l, int r) {
+//    if (r - l <= 1) {
+//        return;
+//    }
+//    int x = a[l + (r-l)/2];
+//    int m = split(a, l, r, x);
+//    qsort(a, l, m);
+//    qsort(a, m+1, r);
+//}
---
+++ algorithms/2_lection/algorithm_Hoara_K/algorithm_Hoara_K.h
@@ -0,0 +1,6 @@
+
+#ifndef VK_MADE_ALGORITHM_HOARA_K_H
+#define VK_MADE_ALGORITHM_HOARA_K_H
+#include <vector>
+int find(std::vector<int> &a, int k, int l, int r);
+#endif //VK_MADE_ALGORITHM_HOARA_K_H
