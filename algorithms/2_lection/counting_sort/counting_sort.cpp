9873a28bebc90b6fda24c18c17ce7512c9991c17
foyreet <foyreet@yandex.ru>
2025-01-26 15:14:43 +0300

    Добавил void restore_sorted_array - функция для восстановления отсортированного массива и изменил ввод вектора на чтение всей строки без надобности вводить размер массива

--- algorithms/2_lection/counting_sort/counting_sort.cpp
+++ algorithms/2_lection/counting_sort/counting_sort.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <limits>
+#include <string>
+#include <sstream>
 
 // void create_vector - функция для создания и заполнения вектора
-void create_vector(std::vector<int>& arr, int& n);
+void create_vector(std::vector<int>& arr);
 
 // std::vector<int> cout_vector - функция для вывода вектора
 void cout_vector(std::vector<int>& cnt);
@@ -17,24 +19,28 @@ int min_element(std::vector<int>& arr, int n);
 // void create_cnt - функция для заполнения массива cnt значениями
 void create_cnt(std::vector<int>& arr, int n, std::vector<int>& cnt, int min_val);
 
+// void restore_sorted_array - функция для восстановления отсортированного массива
+void restore_sorted_array(const std::vector<int>& cnt, std::vector<int>& sorted_arr, int min_val);
 
 int main() {
     std::vector<int> arr;
-    int n = 0;
-    create_vector(arr, n);
-    int max_arr = max_element(arr, n);
-    int min_arr = min_element(arr, n);
+    create_vector(arr);
+    int max_arr = max_element(arr, arr.size());
+    int min_arr = min_element(arr, arr.size());
     int size_cnt = max_arr - min_arr + 1;
     std::vector<int> cnt(size_cnt, 0);
-    create_cnt(arr, n, cnt, min_arr);
-    cout_vector(cnt);
+    create_cnt(arr, arr.size(), cnt, min_arr);
+    std::vector<int> sorted_arr;
+    restore_sorted_array(cnt, sorted_arr, min_arr);
+    cout_vector(sorted_arr);
 }
 
-void create_vector(std::vector<int>& arr, int& n) {
-    std::cin >> n;
-    for (int i = 0; i < n; i++) {
-        int value;
-        std::cin >> value;
+void create_vector(std::vector<int>& arr) {
+    std::string line;
+    std::getline(std::cin, line); // Считываем всю строку
+    std::stringstream ss(line);
+    int value;
+    while (ss >> value) {
         arr.push_back(value);
     }
 }
@@ -71,7 +77,13 @@ void create_cnt(std::vector<int>& arr, int n, std::vector<int>& cnt, int min_val
     }
 }
 
-
+void restore_sorted_array(const std::vector<int>& cnt, std::vector<int>& sorted_arr, int min_val) {
+    for (size_t i = 0; i < cnt.size(); i++) {
+        for (int j = 0; j < cnt[i]; j++) {
+            sorted_arr.push_back(i + min_val);
+        }
+    }
+}
 
 
 
