8e2fcff5ef97a949e429c3f9a4b4c07b4a0e22c5
foyreet <foyreet@yandex.ru>
2025-01-27 10:54:31 +0300

    Убрал прототипы функций и определение функций

--- algorithms/2_lection/counting_sort/counting_sort.cpp
+++ algorithms/2_lection/counting_sort/counting_sort.cpp
@@ -1,26 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <limits>
-#include <string>
-#include <sstream>
-
-// void create_vector - функция для создания и заполнения вектора
-void create_vector(std::vector<int>& arr);
-
-// std::vector<int> cout_vector - функция для вывода вектора
-void cout_vector(std::vector<int>& cnt);
-
-// int max_element - функция для нахождения максимального элемента в векторе
-int max_element(std::vector<int>& arr, int n);
-
-// int min_element - функция для нахождения минимального элемента в векторе
-int min_element(std::vector<int>& arr, int n);
-
-// void create_cnt - функция для заполнения массива cnt значениями
-void create_cnt(std::vector<int>& arr, int n, std::vector<int>& cnt, int min_val);
-
-// void restore_sorted_array - функция для восстановления отсортированного массива
-void restore_sorted_array(const std::vector<int>& cnt, std::vector<int>& sorted_arr, int min_val);
+#include "counting_sort.h"
 
 int main() {
     std::vector<int> arr;
@@ -35,55 +13,12 @@ int main() {
     cout_vector(sorted_arr);
 }
 
-void create_vector(std::vector<int>& arr) {
-    std::string line;
-    std::getline(std::cin, line); // Считываем всю строку
-    std::stringstream ss(line);
-    int value;
-    while (ss >> value) {
-        arr.push_back(value);
-    }
-}
 
-void cout_vector(std::vector<int>& cnt) {
-    for (size_t i = 0; i < cnt.size(); i++) {
-        std::cout << cnt[i] << " ";
-    }
-}
 
-int max_element(std::vector<int>& arr, int n) {
-    int max_val = arr[0];
-    for (int i = 0; i < n; i++) {
-        if (arr[i] >= max_val) {
-            max_val = arr[i];
-        }
-    }
-    return max_val;
-}
 
-int min_element(std::vector<int>& arr, int n) {
-    int min_val = std::numeric_limits<int>::max();
-    for (int i = 0; i < n; i++) {
-        if (arr[i] <= min_val) {
-            min_val = arr[i];
-        }
-    }
-    return min_val;
-}
 
-void create_cnt(std::vector<int>& arr, int n, std::vector<int>& cnt, int min_val) {
-    for (int i = 0; i < n; i++) {
-        cnt[arr[i] - min_val]++;
-    }
-}
 
-void restore_sorted_array(const std::vector<int>& cnt, std::vector<int>& sorted_arr, int min_val) {
-    for (size_t i = 0; i < cnt.size(); i++) {
-        for (int j = 0; j < cnt[i]; j++) {
-            sorted_arr.push_back(i + min_val);
-        }
-    }
-}
+
 
 
 
